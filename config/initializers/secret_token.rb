15625733ab5d8b1013878bd07e8c072c54500e2e
user <rashila@rashila-Lenovo-G550.(none)>
2013-03-25 21:10:48 +0530

    New design

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goglobal::Application.config.secret_token = 'b64bd9412f4d3308fa96cc64fc0443f03913a01e5cabb81a19b29eb2a9cde088dc2cb8b7aea5b73ecf31f12ac08bd84ec945ae6e65ee365aefd7b8bd145b15d0'
